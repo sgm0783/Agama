460082872527eb1b1d4d6ea2303cb79493c5c6c3
David Dawes <virtualsoundnw+github@gmail.com>
2018-07-07 02:12:04 -0700

    Version to 0.3.10, bit of testing (#36)
    * Version to 0.3.10

--- test/spec.js
+++ test/spec.js
@@ -2,6 +2,7 @@ const Application = require('spectron').Application
 const assert = require('assert')
 const electronPath = require('electron') // Require Electron from the binaries included in node_modules.
 const path = require('path')
+const chai = require('chai')
 describe('Application launch', function () {
   this.timeout(30000)
 
@@ -29,10 +30,15 @@ describe('Application launch', function () {
     })
   })
 
-  it('supports VerusCoin selection', function () {
-    this.app.client.getText("document.getElementsByClassName('Select-arrow')[1]").then(function (result) {
-      console.log('The Select-arrow selection returns ' + result)
-    })
+  it('has a native coin list that takes <Tab>', function () {
+    // Wait for the left button for native mode coins is visible
+    this.app.client.element('#react-select-3--value').waitForVisible(3000)
+    // Click on it and hit <Tab> to select the 1st - VerusCoin
+    return this.app.client.element('#react-select-3--value').click().keys("Tab")
+  })
+
+  it('removes the coin selector', function() {
+    return this.app.client.element('#react-select-3--value').waitForVisible(4000, true)
   })
 })
 
