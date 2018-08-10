7144c350d5e0da12eff7c3ef7f201c95a47c8f4f
Michael Filip Toutonghi <michaeltoutjr@Michaels-MacBook.local>
2018-08-09 19:08:57 -0700

    Increased number of displayed transactions

--- routes/shepherd/dashboardUpdate.js
+++ routes/shepherd/dashboardUpdate.js
@@ -288,7 +288,7 @@ module.exports = (shepherd) => {
         if (_call === 'listtransactions') {
           _params = [
             '*',
-            300,
+            5000,
             0
           ];
         }
