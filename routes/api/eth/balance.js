240aa38dc08239a54caf3aa6af11dd5f3121e1ad
pbca26 <pbca26@gmail.com>
2018-11-04 16:17:41 +0300

    eth balance erc20 all balances for a specific address

--- routes/api/eth/balance.js
+++ routes/api/eth/balance.js
@@ -90,6 +90,79 @@ module.exports = (api) => {
     });
   };
 
+  api.eth._balanceERC20All = (address) => {
+    let _balance = {};
+
+    return new Promise((resolve, reject) => {
+      const options = {
+        url: `http://api.ethplorer.io/getAddressInfo/${address}?apiKey=freekey`,
+        method: 'GET',
+      };
+
+      console.log(`http://api.ethplorer.io/getAddressInfo/${address}?apiKey=freekey`);
+
+      request(options, (error, response, body) => {
+        if (response &&
+            response.statusCode &&
+            response.statusCode === 200) {
+          try {
+            const _json = JSON.parse(body);
+
+            if (_json &&
+                _json.address) {
+              console.log(_json.tokens);
+              if (_json.tokens) {
+                //_json.tokens
+                for (let i = 0; i < _json.tokens.length; i++) {
+                  _balance[_json.tokens[i].tokenInfo.symbol] = {
+                    wei: _json.tokens[i].balance,
+                    float: ethers.utils.formatEther(_json.tokens[i].balance.toString()),                    
+                  };
+                }
+                resolve(_balance);
+              }
+            } else {
+              // TODO: loop active erc20 tokens and return 0 balance
+              resolve(_json);
+            }
+          } catch (e) {
+            api.log('ethplorer balance parse error', 'eth.erc20-balance');
+            api.log(e, 'eth.erc20-balance');
+          }
+        } else {
+          api.log(`ethplorer balance error: unable to request http://api.ethplorer.io/getAddressInfo/${address}?apiKey=freekey`, 'eth.erc20-balance');
+        }
+      });
+    });
+  };
+
+  api.get('/eth/erc20/balance/all', (req, res, next) => {
+    const address = req.query.address;
+    const erc20 = req.query.erc20;
+    
+    if (address) {
+      api.eth._balanceERC20All(address)
+      .then((balance) => {
+        const retObj = {
+          msg: 'success',
+          result: balance,
+        };
+    
+        res.end(JSON.stringify(retObj));  
+      });
+    } else {
+      api.eth._balance()
+      .then((balance) => {
+        const retObj = {
+          msg: 'success',
+          result: balance,
+        };
+    
+        res.end(JSON.stringify(retObj));  
+      });
+    }
+  });
+
   api.eth._txcount = () => {
     let _txCount = 0;
     
