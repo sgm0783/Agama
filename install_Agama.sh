129c09cbd0359411fa4ed3e514df6723f24009e3
Lucio Montero <luciric2000@hotmail.com>
2018-11-21 02:08:42 +0100

    Is able to install all only by executing install_Agama.sh in Linux.

--- install_Agama.sh
+++ install_Agama.sh
@@ -13,3 +13,25 @@ git clone https://github.com/KomodoPlatform/EasyDEX-GUI
 cd EasyDEX-GUI/react
 git checkout dev
 npm install
+cd ../../../
+##Detect OS and run the adequate builder
+os=${OSTYPE//[0-9.-]*/}
+
+case "$os" in
+  darwin)
+    echo "I'm a Mac. ¿Do I need to run more steps?."
+    ;;
+
+  msys)
+    echo "I'm Windows using git bash. ¿Do I need to run more steps?.";
+	;;
+  linux)
+    echo "Building on Linux"
+	./build-linux.sh 0.2.44-beta
+	;;
+  *)
+
+  echo "Unknown Operating system $OSTYPE"
+  exit 1
+esac
+
