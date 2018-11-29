0511afcce5ef947e5b2d9bc14ad7dff4acd4482a
Lucio Montero <luciric2000@hotmail.com>
2018-11-29 03:36:42 +0100

    Cmt

--- routes/api/walletlib.js
+++ routes/api/walletlib.js
@@ -49,8 +49,8 @@ module.exports = (api) => {
 		walletinfo.randomSeedConfirm=walletinfo.randomSeed;
 		//walletinfo.ipassphrasegenerationattempt=itime;
 		walletinfo.msg="success";
-	    res.end(JSON.stringify(walletinfo));
 		walletinfo.randomSeedConfirm=walletinfo.randomSeed;
+	    //res.end(JSON.stringify(walletinfo));
 		const isIguana=true;
 		const _wifError = api.auth(walletinfo.randomSeed, isIguana);
 		logininfo={"seed":walletinfo.randomSeed,"result":_wifError?"error":"success"}
